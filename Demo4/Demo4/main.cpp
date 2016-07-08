20be684c3cae503db4cf1d4e20eacd6678768bc1
zhw <18868119485@163.com>
2016-07-08 11:25:42 +0800

    Demo 5: Coordinate System

--- Demo4/Demo4/main.cpp
+++ Demo4/Demo4/main.cpp
@@ -25,6 +25,11 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 // Window dimensions
 const GLuint WIDTH = 800, HEIGHT = 600;
 
+//Objection position
+glm::vec3 viewPos(0.0f, 0.0f, -5.0f);
+GLfloat fov = 45.0f;
+GLfloat aspectRatio = GLfloat(WIDTH) / HEIGHT;
+
 // The MAIN function, from here we start the application and run the game loop
 int main()
 {
@@ -58,38 +63,79 @@ int main()
 
 	// Set up vertex data (and buffer(s)) and attribute pointers
 	GLfloat vertices[] = {
-		// Positions          // Colors           // Texture Coords
-		0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // Top Right
-		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // Bottom Right
-		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // Bottom Left
-		-0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // Top Left 
+		// Positions     // Texture Coordinates
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+		-0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+
+		-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		-0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f
 	};
-	GLuint indices[] = {  // Note that we start from 0!
-		0, 1, 3, // First Triangle
-		1, 2, 3  // Second Triangle
+	
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f, 0.0f, 0.0f),
+		glm::vec3(2.0f, 5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f, 3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f, 2.0f, -2.5f),
+		glm::vec3(1.5f, 0.2f, -1.5f),
+		glm::vec3(-1.3f, 1.0f, -1.5f)
 	};
-	GLuint VBO, VAO, EBO;
+
+	GLuint VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
 	// Position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-	// Color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(1);
+
 	// TexCoord attribute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
 
 	glBindVertexArray(0); // Unbind VAO
 
@@ -133,6 +179,8 @@ int main()
 	SOIL_free_image_data(image);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
+	//enable depth test
+	glEnable(GL_DEPTH_TEST);
 
 	// Game loop
 	while (!glfwWindowShouldClose(window))
@@ -143,19 +191,19 @@ int main()
 		// Render
 		// Clear the colorbuffer
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		//create an animation using matrix transform
-		glm::mat4 trans;
-		GLfloat curtime = glfwGetTime();
-		GLfloat posX = curtime - (GLint(curtime)/2*2 + 1);
-		trans = glm::translate(trans, glm::vec3(posX, posX, 0.0f));
-		trans = glm::rotate(trans, glm::radians((GLfloat)curtime*50.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		//get time, start from 0
+		GLfloat curTime = glfwGetTime();
 
-		glm::mat4 trans1;
-		GLfloat scaleSize = sin(curtime);
-		trans1 = glm::translate(trans1, glm::vec3(-0.5f, 0.5f, 0.0f));
-		trans1 = glm::scale(trans1, glm::vec3(scaleSize, scaleSize, 0));
+		//========
+		// Coordinate System
+		//========
+		glm::mat4 view;
+		view = glm::translate(view, viewPos);
+
+		glm::mat4 projection;
+		projection = glm::perspective(glm::radians(fov), GLfloat(aspectRatio), 0.1f, 100.0f);
 
 		// Activate shader
 		ourShader.Use();
@@ -168,14 +216,24 @@ int main()
 		glBindTexture(GL_TEXTURE_2D, texture2);
 		glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture2"),1);
 
-		GLuint transLoc = glGetUniformLocation(ourShader.Program, "transform");
-		glUniformMatrix4fv(transLoc, 1, GL_FALSE, glm::value_ptr(trans));
+		// transport uniform variable in shader
+		GLuint locView = glGetUniformLocation(ourShader.Program, "view");
+		glUniformMatrix4fv(locView, 1, GL_FALSE, glm::value_ptr(view));
+
+		GLuint locProjection = glGetUniformLocation(ourShader.Program, "projection");
+		glUniformMatrix4fv(locProjection, 1, GL_FALSE, glm::value_ptr(projection));
 
 		// Draw container
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		glUniformMatrix4fv(transLoc, 1, GL_FALSE, glm::value_ptr(trans1));
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		for (GLuint i = 0; i < 10; i++)
+		{
+			glm::mat4 model;
+			model = glm::translate(model, cubePositions[i]);
+			model = glm::rotate(model, glm::radians((curTime*-45.0f)*(i < 3)), glm::vec3(1.0f, 1.0f, 1.0f));
+			GLuint locModel = glGetUniformLocation(ourShader.Program, "model");
+			glUniformMatrix4fv(locModel, 1, GL_FALSE, glm::value_ptr(model));
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 		glBindVertexArray(0);
 
 		// Swap the screen buffers
@@ -184,7 +242,6 @@ int main()
 	// Properly de-allocate all resources once they've outlived their purpose
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
 	// Terminate GLFW, clearing any resources allocated by GLFW.
 	glfwTerminate();
 	return 0;
@@ -193,6 +250,49 @@ int main()
 // Is called whenever a key is pressed/released via GLFW
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
+	GLfloat speed = 0.2f;
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
+	if (key == GLFW_KEY_LEFT && action == GLFW_REPEAT)
+	{
+		fov -= 1.0f;
+	}
+
+	if (key == GLFW_KEY_RIGHT && action == GLFW_REPEAT)
+	{
+		fov += 1.0f;
+	}
+
+	if (key == GLFW_KEY_UP && action == GLFW_REPEAT)
+	{
+		aspectRatio += 0.1f;
+	}
+	if (key == GLFW_KEY_DOWN && action == GLFW_REPEAT)
+	{
+		aspectRatio -= 0.1f;
+	}
+	if (key == GLFW_KEY_W && action == GLFW_REPEAT)
+	{
+		viewPos.y -= speed;
+	}
+	if (key == GLFW_KEY_S && action == GLFW_REPEAT)
+	{
+		viewPos.y += speed;
+	}
+	if (key == GLFW_KEY_D && action == GLFW_REPEAT)
+	{
+		viewPos.x -= speed;
+	}
+	if (key == GLFW_KEY_A && action == GLFW_REPEAT)
+	{
+		viewPos.x += speed;
+	}
+	if (key ==GLFW_KEY_Z && action == GLFW_REPEAT)
+	{
+		viewPos.z += speed;
+	}
+	if (key == GLFW_KEY_X && action == GLFW_REPEAT)
+	{
+		viewPos.z -= speed;
+	}
 }
